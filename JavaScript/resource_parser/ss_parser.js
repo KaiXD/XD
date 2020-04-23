ec9561badc8665468f8d2d76d0bbac9a757de7d0
KaiXD <58248583+KaiXD@users.noreply.github.com>
2020-04-24 05:01:47 +0800

    Update ss_parser.js

--- JavaScript/resource_parser/ss_parser.js
+++ JavaScript/resource_parser/ss_parser.js
@@ -2,6 +2,15 @@
  * README: https://github.com/KaiXD/XD/blob/master/JavaScript/resource_parser/README.md
  */
 
+// $resource, $notify(title, subtitle, message)
+// HTTP reqeust and persistent storage related APIs are not supported in resource parser.
+
+// $resource.link contains the original URL of the resource or the path if the resource is local.
+// $resource.content contains the response(UTF8) from the URL .
+
+// $done({error : "error description"});
+// $done({content : "the modified content"});
+
 "use strict";
 
 function shadowsocks_parser () {
